e7cba906914b87d313b79000c4aba798e6492734
asifahmed0021 <asif.ahmed0021@gmail.com>
2021-07-18 15:34:41 +0530

    add first two sample code files

---
+++ two-fer/src/main/java/Twofer.java
@@ -0,0 +1,13 @@
+public class Twofer {
+    public String twofer(String name) {
+            String ans="One for ";
+            if(name==null || name.equals("")){
+                ans="One for you, one for me.";
+            }
+            else{
+                ans+=name+", one for me.";
+            }
+            return ans;
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
